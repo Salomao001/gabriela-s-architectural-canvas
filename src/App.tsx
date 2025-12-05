fc65649d1d25a5afe602755170d9524517e3a40d
eduardo.jardim <eduardo.salomao.584@gmail.com>
2025-12-05 01:10:00 -0300

    alteração

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gabriela-s-architectural-canvas/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
